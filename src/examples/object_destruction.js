9daf4e93c3efd165fe67a816d545c0a16f83f395
barondas <barondas@oclc.org>
2016-08-15 21:04:37 -0400

    Removing some examples

--- src/examples/object_destruction.js
+++ src/examples/object_destruction.js
@@ -9,18 +9,10 @@ const ObjectDesstruction = () => (
   key3: 'key3'
 }
 
-const data2 = [1, 2, 3, 4, 5]
-
 const {key1} = data
-const [one, two, ...rest] = data2
-const [,,,, lastOne] = data2
 const func1 = ({importantKey}) => importantKey
 
 document.write(key1)
-document.write("<br/>")
-document.write(rest)
-document.write("<br/>")
-document.write(lastOne)
 document.write("<br/>")
 document.write(func1({
   key1: 'key1',
